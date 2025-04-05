6c59afd22fa323118d6b806075efe7f8d081f81b
DiederichSolis <sol22952@uvg.edu.gt>
2025-04-05 16:54:29 -0600

    feat: manifest

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Facturación Electrónica Guatemala FEL',
+    'version': '1.0',
+    'author': 'Tu Nombre',
+    'category': 'Accounting',
+    'summary': 'Integración con FEL Guatemala a través de INFILE',
+    'depends': ['account', 'l10n_gt'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/fel_data.xml',
+        'views/account_move_views.xml',
+        'views/fel_config_views.xml',
+        'views/fel_templates.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
